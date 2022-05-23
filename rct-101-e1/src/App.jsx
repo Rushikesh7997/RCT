f51bbf0dd9fe6d316dd2f68d90bc6b2a53cebe78
Rushikesh7997 <pardhikar26@gmail.com>
2022-05-23 16:57:11 +0530

    solved

---
+++ rct-101-e1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import'./App.css'
+
+import TaskApp from "./components/TaskApp";
+
+function App() {
+  return <div className="container">
+    <TaskApp/>
+  </div>;
+}
+
+export default App;
