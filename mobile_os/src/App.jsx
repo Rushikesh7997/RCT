7fe788d3982839b3db638a29a8a20da8af9c9cf5
Rushikesh7997 <pardhikar26@gmail.com>
2022-05-27 22:51:07 +0530

    update os and nav

---
+++ mobile_os/src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  const food = "Pizza";
+  const age = "19";
+  const arr1 = ["Android", "Blackberry", "iphone", "Windows Phone"];
+  const arr2 = ["Samsung", "HTC", "Micromax", "Apple"]
+  return (
+    <div className="App">
+      <div>
+        <ul>
+          <h2>Mobile Operating System</h2>
+          {arr1.map((e) => {
+            return <li><Skill arr1={e}/> </li>;
+          })}
+        </ul>
+        <ul>
+          <h2>Mobile Manufacturers</h2>
+          {arr2.map((e) => {
+            return <li><Skillss arr2={e}/> </li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Skill (props){
+  return <p> {props.arr1} </p>
+}
+function Skillss (props){
+  return <p> {props.arr2} </p>
+}
+
+export default App;
